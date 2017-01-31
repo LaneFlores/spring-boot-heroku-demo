e80bfdab53c5a447149abfbfd81e1963a40da549
LaneFlores <mflores@star2star.com>
2017-01-31 15:38:07 -0500

    Dev Update
    Creating the dev branch and separating from production.

--- src/main/java/com/heroku/demo/DebugController.java
+++ src/main/java/com/heroku/demo/DebugController.java
@@ -1,4 +1,15 @@
 package com.heroku.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/debug")
 public class DebugController {
+
+
+    @RequestMapping
+    public String debug() {
+        return "debugging page?";
+    }
 }
