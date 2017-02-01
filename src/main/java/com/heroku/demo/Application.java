d968989820542351fb82afd8e2654fab71b584eb
LaneFlores <mflores@star2star.com>
2017-02-01 15:59:07 -0500

    MySQL
    Starting to integrate MySQL.

--- src/main/java/com/heroku/demo/Application.java
+++ src/main/java/com/heroku/demo/Application.java
@@ -17,8 +17,15 @@ package com.heroku.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ComponentScan(basePackages = {
+        "com.heroku.demo"
+})
 public class Application {
 
     public static void main(String[] args) {
