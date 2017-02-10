eb6f19975c9bbe31dbe9689e4ad26b3e02270570
LordKahra <lordkahra@gmail.com>
2017-02-10 14:34:58 -0500

    Tests for Codeship

--- src/test/java/com/heroku/demo/ApplicationTest.java
+++ src/test/java/com/heroku/demo/ApplicationTest.java
@@ -1,5 +1,6 @@
 package com.heroku.demo;// PURPOSE: 
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,10 @@ public class ApplicationTest {
         // This is a test, just trying to see if it runs.
         log.info("This is a test run.");
         System.out.println("Test run?!");
+    }
+
+    @Test public void failingExample() {
+        System.out.println("This is going to intentionally fail.");
+        Assert.assertTrue(false);
     }
 }
