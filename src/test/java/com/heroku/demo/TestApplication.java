f5160dcab17593d9f6588334a198d1d626a7f2a6
LordKahra <lordkahra@gmail.com>
2017-02-10 13:48:39 -0500

    CORS

---
+++ src/test/java/com/heroku/demo/TestApplication.java
@@ -0,0 +1,15 @@
+package com.heroku.demo;// PURPOSE: 
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class TestApplication {
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void testExample() {
+        // This is a test, just trying to see if it runs.
+    }
+}
